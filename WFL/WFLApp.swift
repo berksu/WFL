907b0181e03c7c8f2af5bf8b676838c4f79a296e
Berksu Kısmet <berksukismet@gmail.com>
2021-12-20 22:33:54 +0300

    Initial Commit

---
+++ WFL/WFLApp.swift
@@ -0,0 +1,17 @@
+//
+//  WFLApp.swift
+//  WFL
+//
+//  Created by Berksu Kısmet on 20.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WFLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
